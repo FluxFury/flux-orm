1791cbdaea733485a5517bb04d41d412fc2707b3
JumbikTank <167221137+JumbikTank@users.noreply.github.com>
2024-09-17 19:09:12 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from uuid import UUID
---
+++ __main__.py
@@ -0,0 +1,8 @@
+import asyncio
+
+from models.database import *
+from models.cs.main_page_models import *
+from models.cs.match_page_models import *
+
+if __name__ == '__main__':
+    asyncio.run(main())
